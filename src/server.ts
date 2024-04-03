13c8b0c76fc6aa3334dc98a9d386cac175cc43f1
Ryan-lmm <ryanlmm4@gmail.com>
2024-04-02 21:08:14 -0300

    register user route created

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { fastify } from 'fastify'
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 import { createEvent } from './routes/create-event.js';
+import { registerForEvent } from './routes/register-event.js';
 
 const app = fastify()
 
@@ -8,6 +9,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
+app.register(registerForEvent)
 
 app.listen({
   port: 3333
